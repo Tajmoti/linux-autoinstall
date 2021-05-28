440ff55f331fe66cd0b15f16e366985f943c746a
Timotej Šulík <timotej.sulik@loyaltek.com>
2021-05-28 15:17:06 +0200

    Attempt

--- setup.sh
+++ setup.sh
@@ -3,9 +3,14 @@
 # Location for temporary install files
 workdir="/tmp/tajmoinstall"
 
+# Software URLs
 hamachi_url="https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb"
 insync_url="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.4.0.40973-buster_amd64.deb"
 netextender_url="https://software.sonicwall.com/NetExtender/NetExtender.Linux-10.2.824.x86_64.tgz"
+
+# Commands
+cmd_wget="wget -q"
+cmd_apt="apt -qq"
 
 # APT packages to install
 apt_packages="\
@@ -60,19 +65,19 @@ mkdir "$workdir" && pushd "$workdir"
 
 # Update APT cache
 echo "Updating APT cache"
-sudo apt -qq update
+sudo $cmd_apt update
 
 # Install APT packages
 echo "Installing APT packages"
-sudo apt -qq install -y $apt_packages
+sudo $cmd_apt install -y $apt_packages
 
 # Uninstall useless software
 echo "Uninstalling APT crapware"
-sudo apt -qq purge -y --autoremove $crapware
+sudo $cmd_apt purge -y --autoremove $crapware
 
 # Upgrade packages
 echo "Upgrading APT packages"
-sudo apt -qq upgrade -y
+sudo $cmd_apt upgrade -y
 
 # Flatpak
 echo "Setting up Flatpak"
@@ -82,20 +87,20 @@ flatpak install -y $flatpaks
 
 # Custom software
 echo "Downloading custom software"
-wget -O hamachi.deb "$hamachi_url"
-wget -O teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-wget -O insync.deb "$insync_url"
-wget -O jb-toolbox.tar.gz https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
-wget -O netextender.tar.gz "$netextender_url"
+$cmd_wget -O hamachi.deb "$hamachi_url"
+$cmd_wget -O teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+$cmd_wget -O insync.deb "$insync_url"
+$cmd_wget -O jb-toolbox.tar.gz https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
+$cmd_wget -O netextender.tar.gz "$netextender_url"
 
 echo "Unpacking custom software"
 tar -xf jb-toolbox.tar.gz
 tar -xf netextender.tar.gz
 
 echo "Installing custom software"
-sudo apt -qq install -y ./hamachi.deb
-sudo apt -qq install -y ./teamviewer.deb
-sudo apt -qq install -y ./insync.deb
+sudo $cmd_apt install -y ./hamachi.deb
+sudo $cmd_apt install -y ./teamviewer.deb
+sudo $cmd_apt install -y ./insync.deb
 jetbrains-toolbox*/jetbrains-toolbox
 pushd netExtenderClient
 sudo ./install
