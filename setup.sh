35740ebae0ae8db4176e46efc68293cff13e8a3a
Timotej Šulík <timotej.sulik@gmail.com>
2022-03-23 22:14:17 +0100

    Fetch latest JetBrains toolbox version to install

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ https://download.teamviewer.com/download/linux/teamviewer_amd64.deb \
 "
 
 # Generic software URLs
-jb_toolbox_url="https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.20.8352.tar.gz"
+jb_toolbox_url=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' | sed -rn 's|^.*"linux":\{"link":"([^"]*).*$|\1|p')
 netextender_url="https://software.sonicwall.com/NetExtender/NetExtender.Linux-10.2.824.x86_64.tgz"
 
 
